db5d687d04e149a1fbf7ae0074b40df714a8c677
Alex Milkovsky <a.milkovsky@gmail.com>
2014-09-12 23:38:15 +0200

    initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-ie_placeholders
-===============
-
-Add support of placeholder attribute in IE
---
+++ jquery.ieplaceholders.js
@@ -0,0 +1,127 @@
+(function($) {
+
+  var prototype = $.fn;
+  iePlaceholder = prototype.iePlaceholder = function() {
+    var input = $(this);
+
+    // Check normal browsers.
+    var isOperaMini = Object.prototype.toString.call(window.operamini) == '[object OperaMini]';
+    var isInputSupported = 'placeholder' in document.createElement('input');
+    var isTextareaSupported = 'placeholder' in document.createElement('textarea');
+    // Exit execution for normal browsers.
+    var text = input.attr('placeholder');
+    if (isInputSupported && isTextareaSupported && !isOperaMini) {
+      //return;
+      input.attr('placeholder', '');
+    }
+
+    input.parrent = input.parent();
+    input.parrent.addClass('placeholder-parrent');
+
+    // Prepare placeholder.
+
+    input.placeholder = $("<div/>", {
+      'class': 'placeholder-overlay',
+      'text': text
+    });
+    input.placeholder.input = input;
+    // Copy placeholder styles.
+    var styles = ['font-size', 'font-weight', 'font-family'];
+    $.each(styles , function(item, value) {
+      input.placeholder.css(value, input.css(value));
+    });
+    // Calculate placeholder position.
+    setPosition(input);
+    input.after(input.placeholder);
+
+    // Toggle placeholder.
+    togglePlaceholder(input);
+    input.bind('input', function() {
+      togglePlaceholder(input);
+    });
+    input.placeholder.click( function() {
+      togglePlaceholder(input);
+      input.focus();
+    });
+
+    // Highlight placeholder on focus.
+    input.focus(function() {
+      input.placeholder.addClass('highlighted');
+    });
+    // Unhighlight placeholder on blur.
+    input.blur(function() {
+      input.placeholder.removeClass('highlighted');
+      togglePlaceholder(input);
+    });
+
+    /**
+     * Toggle placeholder.
+     */
+    function togglePlaceholder(input) {
+      if (input.val() !== '') {
+        input.placeholder.hide();
+      }
+      else {
+        input.placeholder.show();
+      }
+    };
+
+    /**
+     * Calculate input placeholder position.
+     *
+     * @returns array position
+     */
+    function setPosition(input) {
+      var left = 0, top = 0;
+      if(paddingLeft = parseInt(input.parrent.css('padding-left'))) {
+        left += paddingLeft;
+      }
+      if(margingLeft = parseInt(input.parrent.css('marging-left'))) {
+        left += paddingLeft;
+      }
+      if(paddingLeft = parseInt(input.css('padding-left'))) {
+        left += paddingLeft;
+      }
+      if(margingLeft = parseInt(input.css('marging-left'))) {
+        left += paddingLeft;
+      }
+      if(paddingTop = parseInt(input.parrent.css('padding-top'))) {
+        top += paddingTop;
+      }
+      if(margingTop = parseInt(input.parrent.css('marging-top'))) {
+        top += margingTop;
+      }
+      if(paddingTop = parseInt(input.css('padding-top'))) {
+        top += paddingTop;
+      }
+      if(margingTop = parseInt(input.css('marging-top'))) {
+        top += margingTop;
+      }
+      input.placeholder.css('top', top);
+      input.placeholder.css('left', left);
+    };
+  }
+
+  Drupal.behaviors.iePlaceholders = {
+    attach: function(context, settings) {
+      var include = settings.ie_placeholders.include;
+      $(include, context).each(function() {
+
+        var input = $(this);
+        var form = input.closest('form');
+        var label = form.find('label[for=' + this.id + ']');
+        // Clone label
+        var labelClone = jQuery.extend({}, label);
+        labelClone.find('.form-required').remove();
+        // Set placeholder text from label.
+        if (input.attr('placeholder') == undefined || input.attr('placeholder') == '') {
+          input.attr('placeholder', $.trim(labelClone.text()));
+          label.hide();
+        }
+
+        input.iePlaceholder();
+      });
+    }
+  };
+
+})(jQuery);
